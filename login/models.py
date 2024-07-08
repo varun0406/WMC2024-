b83de54f23303c3db793ef649f29800a6bdaf841
AomKapadia <kapadiaaom78@gmail.com@ahduni.edu.in>
2024-07-09 00:10:03 +0530

    profile added

--- login/models.py
+++ login/models.py
@@ -5,9 +5,11 @@ class Profile(models.Model):
     user_id = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    picture = models.URLField()
+    picture = models.URLField(default='/img')
     KarmaPoints = models.IntegerField(default=0)
     Account_Balance = models.IntegerField(default=0)
     user_type = models.CharField(max_length=100, default="User")
     Membership_license = models.CharField(max_length=100,default="None")
     Total_Donation = models.IntegerField(default=0)
+    def __str__(self):
+        return self.user_id
--- login/views.py
+++ login/views.py
@@ -1,40 +1,69 @@
 from django.shortcuts import render
 import json
 from django.http import HttpResponseRedirect
+from django.shortcuts import render,HttpResponse,redirect,get_object_or_404
 
 from django.contrib.auth import logout as django_logout
 from decouple import config
 from login.models import Profile
-from django import forms
+from django.http import Http404
 
 def ProfileConfig(request):
-    form =forms(Profile)
-    
+    return render()
             
     
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    user = request.user
+    if user.is_authenticated:
+        auth_0_user = user.social_auth.get(provider='auth0')
+        user_id= auth_0_user.uid
+        try:
+            user_profile = Profile.objects.get(user_id=user_id)
+        except Profile.DoesNotExist:
+            user_profile = None
+    else:
+        user_id= None
+        user_profile = None
+    params={
+        'user_ID':user_id,
+        'user_profile':user_profile
+        }
+    return render(request,'index.html',params)
+        
 
 def profile(request):
-    user = request.user
-    auth_0_user = user.social_auth.get(provider='auth0')
-    user_data= {
-        'user_id': auth_0_user.uid,
-        "name": user.first_name,
-        "email": user.email,
-        "picture": auth_0_user.extra_data['picture']}
-    context ={
-        "user_data": user_data,
-        "auth_0_user": auth_0_user,
-        
-    }
-    print(context)
-    return render(request, 'profile.html', context)
+    user=request.user
+    if user.is_authenticated:
+        auth_0_user = user.social_auth.get(provider='auth0')
+        user_id= auth_0_user.uid
+        try:
+            user_profile = Profile.objects.get(user_id=user_id)
+        except Profile.DoesNotExist:
+            raise Http404("User profile does not exist")
+        context={
+            'user_profile':user_profile
+        }
+        return render(request, 'profile.html',context)
+    else:
+        raise Http404("User not logged in")
 def logout(request):
     django_logout(request)
     domain= config("APP_DOMAIN")
     client_id = config("APP_CLIENTID")
     return_to = "http://localhost:8000"
     return HttpResponseRedirect (f"https://{domain}/v2/logout?client_id={client_id}&returnTo={return_to}")
+def Create_Profile(request):
+    if request.method=='POST':
+        user=request.user
+        auth_0_user = user.social_auth.get(provider='auth0')
+        user_name=request.POST.get('name')
+        user_email=request.POST.get('user-email')
+        profile, created=Profile.objects.get_or_create(user)
+        profile.user_id=auth_0_user.uid
+        profile.name=user_name
+        profile.email=user_email
+        profile.save()
+        return redirect('/profile')
+    return render(request, 'Create_Profile.html')
     
